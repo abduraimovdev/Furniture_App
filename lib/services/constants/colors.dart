93226db745bebb1f8c0f05be17049e16c6b08b08
abduraimovdev <kenzo778118@gmail.com>
2023-05-31 11:26:56 +0500

    updated

---
+++ lib/services/constants/colors.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+enum CustomColors {
+  c303030("0xFF303030", Color(0xFF303030)),
+  c909090("0xFF909090", Color(0xFF909090)),
+  c606060("0xFF606060", Color(0xFF606060)),
+  cB4916C("0xFFB4916C", Color(0xFFB4916C)),
+  cE4CBAD("0xFFE4CBAD", Color(0xFFE4CBAD)),
+  cE0E0E0("0xFFE0E0E0", Color(0xFFE0E0E0)),
+  c808080("0xFF808080", Color(0xFF808080)),
+  cB2B2B2("0xFFB2B2B2", Color(0xFFB2B2B2)),
+  cBDBDBD("0xFFBDBDBD", Color(0xFFBDBDBD)),
+  cF2994A("0xFFF2994A", Color(0xFFF2994A)),
+  cEB5757("0xFFEB5757", Color(0xFFEB5757)),
+  cF0F0F0("0xFFF0F0F0", Color(0xFFF0F0F0)),
+  c27AE60("0xFF27AE60", Color(0xFF27AE60)),
+  cF2C94C("0xFFF2C94C", Color(0xFFF2C94C));
+
+  const CustomColors(this.title, this.color);
+  final String title;
+  final Color color;
+}
---
+++ lib/services/constants/images.dart
@@ -0,0 +1,33 @@
+
+enum CustomImage {
+  card("assets/images/img_card.png"),
+  check("assets/images/img_check.png"),
+  logo("assets/images/img_logo.png"),
+  onBoarding("assets/images/img_onboarding.png"),
+
+  //Persons
+
+  person("assets/images/img_person.png"),
+  person2("assets/images/img_person_1.png"),
+  person3("assets/images/img_person_2.png"),
+
+
+  // Products
+  product("assets/images/img_product.png"),
+  product1("assets/images/img_product_1.png"),
+  product2("assets/images/img_product_2.png"),
+  product3("assets/images/img_product_3.png"),
+  product4("assets/images/img_product_4.png"),
+  product5("assets/images/img_product_5.png"),
+  product6("assets/images/img_product_6.png"),
+  product7("assets/images/img_product_7.png"),
+  product8("assets/images/img_product_8.png"),
+  product9("assets/images/img_product_9.png"),
+  product10("assets/images/img_product_10.png"),
+  product11("assets/images/img_product_11.png"),
+  product12("assets/images/img_product_12.png"),
+  product13("assets/images/img_product_13.png");
+
+  const CustomImage(this.img);
+  final String img;
+}
