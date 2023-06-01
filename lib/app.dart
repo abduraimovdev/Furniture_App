c3acf1d2b0e142ad2a30cfd2a2707c4a921b168b
abduraimovdev <kenzo778118@gmail.com>
2023-06-01 15:45:08 +0500

    Updated

--- lib/app.dart
+++ lib/app.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:furniture_app/screens/boarding_screen.dart';
 import 'package:furniture_app/services/app_routes.dart';
 import 'package:furniture_app/services/theme/app_theme.dart';
 
